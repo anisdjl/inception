f999793e62050b2184d49c3ef992e304cdd059b9
anisdjl <adjelili954@gmail.com>
2026-07-15 16:38:02 +0200

    mandatory part done

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME = inception
 
-DOCKER_COMPOSE = requirements/docker-compose.yml
+DOCKER_COMPOSE = srcs/docker-compose.yml
 DATA_DIR = /home/adjelili/data
 
 all: build
@@ -9,3 +9,17 @@ build:
 	@mkdir -p $(DATA_DIR)/wordpress
 	@mkdir -p $(DATA_DIR)/mariadb
 	docker compose -f $(DOCKER_COMPOSE) up --build -d
+
+down:
+	docker compose -f $(DOCKER_COMPOSE) down
+
+clean: down
+	docker system prune -a -f
+
+fclean: clean
+	docker volume rm $(docker volume ls -q) 2>/dev/null || true
+	@sudo rm -rf $(DATA_DIR)
+
+re: clean build
+
+.PHONY: all build down clean fclean re
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -10,12 +10,14 @@ mariadbd-safe --datadir='/var/lib/mysql' & # launch mariadb
 
 sleep 5 # allow the service to start and not execute the commands before it starts
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" # will create a database if it doesn't exists named after the name stored in the env var SQL_DATABASE
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" # create a new user with a name and a passeword
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';" # give the rights to the user
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" # we set a password to the root
-mysql -e "FLUSH PRIVILEGES;" # the flush is like a refresh (f5) so the DB has the latest version of the modifications we just made
+if mysql -u root -e "status" &>/dev/null; then
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" # will create a database if it doesn't exists named after the name stored in the env var SQL_DATABASE
+    mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" # create a new user with a name and a passeword
+    mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';" # give the rights to the user
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" # we set a password to the root
+    mysql -e "FLUSH PRIVILEGES;" # the flush is like a refresh (f5) so the DB has the latest version of the modifications we just made
+fi
 
 mysqladmin -u root -p"${SQL_ROOT_PASSWORD}" shutdown
 
-exec mariadb-safe
+exec mariadbd-safe
